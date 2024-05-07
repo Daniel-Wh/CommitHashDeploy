c7c3bc0b4c3f3020e9fdfc26d54e74f7db822806
Daniel Whitney <whitneyd.dev@gmail.com>
2024-05-06 20:50:46 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -24,6 +24,9 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
+        <span>
+          This text is specific to this PR
+        </span>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
