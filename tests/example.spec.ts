101f97eca673943db4f94cbf6caf53245d21d16f
Daniel Whitney <whitneyd.dev@gmail.com>
2024-05-07 22:29:48 -0400

    removed optional setting

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,11 +1,9 @@
 import { test, expect } from '@playwright/test';
 
 test('has title', async ({ page, context }) => {
-  console.log('CI:', process.env.CI);
-  console.log('XSOURCECOOKIE:', process.env.XSOURCECOOKIE);
   if (process.env.CI && process.env.XSOURCECOOKIE) {
     const cookie = process.env.XSOURCECOOKIE
-    await context.addCookies([{ name: 'X-source', value: cookie, domain: 'devopswithme.net' }]);
+    await context.addCookies([{ name: 'X-source', value: cookie }]);
   }
   await page.goto('/')
   // Expect a title "to contain" a substring.
