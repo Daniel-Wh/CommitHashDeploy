a2606d6292b7415e103d8681e36906633c544ab8
Daniel Whitney <whitneyd.dev@gmail.com>
2024-05-07 22:09:40 -0400

    testing cookie

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,7 +1,8 @@
 import { test, expect } from '@playwright/test';
 
 test('has title', async ({ page, context }) => {
-
+  console.log('CI:', process.env.CI);
+  console.log('XSOURCECOOKIE:', process.env.XSOURCECOOKIE);
   if (process.env.CI && process.env.XSOURCECOOKIE) {
     const cookie = process.env.XSOURCECOOKIE
     await context.addCookies([{ name: 'X-source', value: cookie }]);
