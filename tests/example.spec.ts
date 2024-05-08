035a300b5962f44626ea4de2738513bd4e717e8f
Daniel Whitney <whitneyd.dev@gmail.com>
2024-05-07 22:34:37 -0400

    fix

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -3,7 +3,7 @@ import { test, expect } from '@playwright/test';
 test('has title', async ({ page, context }) => {
   if (process.env.CI && process.env.XSOURCECOOKIE) {
     const cookie = process.env.XSOURCECOOKIE
-    await context.addCookies([{ name: 'X-source', value: cookie }]);
+    await context.addCookies([{ name: 'X-source', value: cookie, url: process.env.BASE_URL ?? 'http://localhost:5173' }]);
   }
   await page.goto('/')
   // Expect a title "to contain" a substring.
