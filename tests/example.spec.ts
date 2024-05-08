75cbe72447f83c0185742a554235f0129012f25c
Daniel Whitney <whitneyd.dev@gmail.com>
2024-05-07 22:51:26 -0400

    add playwright and ci action (#4)
    * add playwright and ci action
    
    * fix tests
    
    * testing cookie
    
    * settings and validating vars
    
    * again
    
    * removed optional setting
    
    * fix
    
    * Update build.yml
    
    * test cookie
    
    * removed tests - validated in ci

---
+++ tests/example.spec.ts
@@ -0,0 +1,12 @@
+import { test, expect } from '@playwright/test';
+
+test('has title', async ({ page, context }) => {
+  if (process.env.CI && process.env.XSOURCECOOKIE) {
+    const cookie = process.env.XSOURCECOOKIE
+    await context.addCookies([{ name: 'X-source', value: cookie, url: process.env.BASE_URL ?? 'http://localhost:5173' }]);
+  }
+  await page.goto('/')
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle("Vite + React + TS");
+});
+
