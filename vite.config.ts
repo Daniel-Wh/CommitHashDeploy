e6498a9960a9b9e997f447eaf8655162119352e9
Daniel Whitney <whitneyd.dev@gmail.com>
2024-05-11 10:06:39 -0400

    Update vite.config.ts (#7)

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  assetsInclude: ["**/*.svg"],
+  build: {
+    assetsInlineLimit: 0,
+  },
   test: {
     environment: 'jsdom',
     exclude: [
